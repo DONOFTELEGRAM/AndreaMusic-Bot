e36b37d68d6d3fabea6e9812da232f6442dddea8
ʄɽɑɳƙꍟɳʂʈꍟꀤɳ™ ❬❬kiռց ૨ip⛦❭❭ <64843897+DONOFTELEGRAM@users.noreply.github.com>
2023-06-12 20:41:24 +0530

    Update config.py
    Signed-off-by:  ʄɽɑɳƙꍟɳʂʈꍟꀤɳ™ ❬❬kiռց ૨ip⛦❭❭ <64843897+DONOFTELEGRAM@users.noreply.github.com>

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 OWNER_ID = int(getenv("OWNER_ID"))
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/ea5d961c346b360882aae.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/2987a88dbc3e0803eeae7.jpg")
+START_IMG = getenv("START_IMG", "https://te.legra.ph/file/2987a88dbc3e0803eeae7.jpg")
 
 SESSION = getenv("SESSION", None)
 
